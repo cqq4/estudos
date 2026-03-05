3ef4b669f8f88deb5aea9c2424361af6599c4b4d
k <fabioxyz2005@gmail.com>
2026-03-05 20:32:03 -0300

    z

---
+++ c++_avulsos/gcc_teste/1b.cpp
@@ -0,0 +1,18 @@
+# 0 "1.cpp"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 0 "<command-line>" 2
+# 1 "1.cpp"
+# 1 "1a.cpp" 1
+int main(int a, int b){
+    return a + b;
+
+}
+# 2 "1.cpp" 2
+int main(void){
+    int a = 0;
+    int b = 0;
+    int c = 1a(a,b);
+    return 0;
+}
